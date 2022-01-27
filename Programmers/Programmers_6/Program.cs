893139f42f19c4843dcac148c32a8fd6be91ab37
kmc94 <money94611@naver.com>
2022-01-27 21:42:25 +0900

    내적(https://programmers.co.kr/learn/courses/30/lessons/70128?language=csharp)

---
+++ Programmers/Programmers_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 내적 (https://programmers.co.kr/learn/courses/30/lessons/70128?language=csharp)
+namespace Programmers_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3, 4 };
+            int[] b = new int[] { -3, -1, 0, 2 };
+
+            Console.WriteLine(solution(a, b));
+        }
+
+        public static int solution(int[] a, int[] b)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                answer += a[i] * b[i];
+            }
+
+            // Linq
+            a.Zip(b, (t1, t2) => t1 * t2).Sum();
+
+            return answer;
+        }
+    }
+}
